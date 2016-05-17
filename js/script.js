05e7b254010ad542427923a8773dcb8b2a5a9191
dylancbailey <8r7.uTa!>
2016-05-17 12:22:25 -0400

    Redesign

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+    // Fades out header on scroll
+    $(window).scroll(function() {
+        $(".top").css("opacity", 1 - $(window).scrollTop() / 600);
+    });
+
+    // Smooth scrolls
+    $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
+});
